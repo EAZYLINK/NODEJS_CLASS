1cb22b33826f5f85a135b361a1be68721634139f
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2023-07-05 04:35:08 +0100

    initial commit

---
+++ obj.js
@@ -0,0 +1,8 @@
+const data = {
+    "name": "John",
+    "SN": 1,
+    "email": "example.com"
+}
+
+// console.log(data.email) //using dot operator to access an obj value
+// console.log(data['email']) //using square bracket to access an obj value
---
+++ script.js
@@ -0,0 +1,50 @@
+const PI = 3.14159265359;
+
+// console.log("The value of PI is " + PI + "."); //this is concantenation
+// console.log(`The value of PI is ${PI}.`); //this is interpolation
+
+n = 5;
+
+console.log(n == 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
